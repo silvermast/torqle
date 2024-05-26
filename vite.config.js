ff6a97af796217ae48b7bd50d4ebe66e19cff4d3
Jason <jason@silvermast.io>
2024-05-26 14:11:26 -0700

    compiling but not fully functional

--- vite.config.js
+++ vite.config.js
@@ -21,19 +21,23 @@ export default defineConfig(async () => ({
   server: {
     port: 1420,
     strictPort: true,
+    watch: {
+      // 3. tell vite to ignore watching `src-tauri`
+      ignored: ["**/src-tauri/**"],
+    },
   },
   // to make use of `TAURI_DEBUG` and other env variables
   // https://tauri.studio/v1/api/config#buildconfig.beforedevcommand
-  envPrefix: ["VITE_", "TAURI_"],
-  build: {
-    // Tauri supports es2021
-    target: process.env.TAURI_PLATFORM == "windows" ? "chrome105" : "safari13",
-    // don't minify for debug builds
-    minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
-    // produce sourcemaps for debug builds
-    sourcemap: !!process.env.TAURI_DEBUG,
-  },
-  test: {
-    include: ['src/**/*.spec.js'],
-  },
+  // envPrefix: ["VITE_", "TAURI_"],
+  // build: {
+  //   // Tauri supports es2021
+  //   target: process.env.TAURI_PLATFORM == "windows" ? "chrome105" : "safari13",
+  //   // don't minify for debug builds
+  //   minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
+  //   // produce sourcemaps for debug builds
+  //   sourcemap: !!process.env.TAURI_DEBUG,
+  // },
+  // test: {
+  //   include: ['src/**/*.spec.js'],
+  // },
 }));
